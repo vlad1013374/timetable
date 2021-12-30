3186ecfee44980cfebe8cc7c3ee901e41f152cdf
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2021-12-30 23:43:43 +0700

    Update 005.sql

--- migrations/005.sql
+++ migrations/005.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `config` ADD `param_ru` VARCHAR(60) NOT NULL AFTER `param`;
+UPDATE `config` SET `param_ru` = 'Время автосейва' WHERE `config`.`param` = 'autosavePeriodInMinutes';
+UPDATE `config` SET `param_ru` = 'Шрифт' WHERE `config`.`param` = 'font';
+UPDATE `config` SET `param_ru` = 'Размер шрифта' WHERE `config`.`param` = 'fontSize';
