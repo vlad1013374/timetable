786d5397f48f2f7159762cda3657396493162140
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2021-12-31 00:24:47 +0700

    Update 005.sql

--- migrations/005.sql
+++ migrations/005.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `config` ADD `param_ru` VARCHAR(60) NOT NULL AFTER `param`;
-UPDATE `config` SET `param_ru` = 'Время автосейва' WHERE `config`.`param` = 'autosavePeriodInMinutes';
+UPDATE `config` SET `param_ru` = 'Время автосейва(минуты)' WHERE `config`.`param` = 'autosavePeriodInMinutes';
 UPDATE `config` SET `param_ru` = 'Шрифт' WHERE `config`.`param` = 'font';
 UPDATE `config` SET `param_ru` = 'Размер шрифта' WHERE `config`.`param` = 'fontSize';
 UPDATE `config` SET `param_ru` = 'Позиция меток' WHERE `config`.`param` = 'positionFlagsLeft';
