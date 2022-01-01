0332151fac4afb7d94996944bf968680db617845
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2022-01-01 21:29:49 +0700

    Update pub.js

--- pub/includes/pub.js
+++ pub/includes/pub.js
@@ -1,26 +1,26 @@
 
 
-
 for (var i = 0; i <= timetable.length - 1; i++){
 	$("tbody td").each(function() {
 		if($(this).attr("data-class-id") == timetable[i].class_id && 
 		   $(this).attr("data-lesson-id") == timetable[i].lesson_id && 
 		   $(this).attr("data-day") == timetable[i].day){
 		   	if($(this).hasClass("teacher")){
-		   		$(this).append('<div>'+timetable[i].name+'</div>');
+		   		$(this).append('<div class="flag-'+timetable[i].flags+'">'+timetable[i].name+'</div>');
+		   	
 		   		
 		   	}else if($(this).hasClass("subject")){
-		   		$(this).append('<div>'+timetable[i].short_name+'</div>');
+		   		$(this).append('<div class="flag-'+timetable[i].flags+'">'+timetable[i].short_name+'</div>');
 		   		
 
 		   	}else if($(this).hasClass("classroom")){
-		   		$(this).append('<div>'+timetable[i].room_id+'</div>');
+		   		$(this).append('<div class="flag-'+timetable[i].flags+'">'+timetable[i].room_id+'</div>');
+		   		
 		   		
 		   	}
-			$(this).addClass("flag-"+timetable[i].flags);
+			/*$(this).addClass("flag-"+timetable[i].flags);*/
 		}
 	})
 }
-
 
 
