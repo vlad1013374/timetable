96fcd17e91eab8eb369dfc4404cd958fa5a86b82
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2022-01-02 21:55:04 +0700

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -3,6 +3,7 @@ require '../connection/db.php';
 require "model.php";
 
 $json_week = $_POST['week'];
+$week_active = $_POST['weekActive'];
 if (isset($json_week)) {
 	/*add_week(json_decode($json_week));*/
 	if(add_week($json_week)){
